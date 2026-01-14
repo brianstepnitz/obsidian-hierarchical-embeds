9c4b6512eb112fa5dd47fb0187238889b81a5c44
Brian Stepnitz <brian.stepnitz@gmail.com>
2026-01-13 19:40:21 -0500

    Getting my feet wet
    Getting my feet wet

--- src/main.ts
+++ src/main.ts
@@ -1,80 +1,28 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import {Plugin} from 'obsidian';
+import {DEFAULT_SETTINGS, HierarchicalEmbedsSettings, HierarchicalEmbedsSettingTab} from "./settings";
 
-// Remember to rename these classes and interfaces!
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HierarchicalEmbedsPlugin extends Plugin {
+	settings: HierarchicalEmbedsSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('dice', 'Sample', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-modal-simple',
-			name: 'Open modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
+		this.registerMarkdownPostProcessor((element, context) => {
+			const internalEmbed = element.querySelector(".internal-embed");
+			if (internalEmbed) {
+				observeForMarkdownEmbed(element);
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			new Notice("Click");
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
+		this.addSettingTab(new HierarchicalEmbedsSettingTab(this.app, this));
 	}
 
 	onunload() {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<HierarchicalEmbedsSettings>);
 	}
 
 	async saveSettings() {
@@ -82,18 +30,57 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+function observeForElementsAdded(element: HTMLElement) {
+	const markdownEmbedObserver = new MutationObserver((records, observer) => {
+		for (const record of records) {
+			if (record.target instanceof HTMLElement) {
+				const markdownEmbedAncestors = findAllMarkdownEmbedAncestors(record.target);
+				console.debug("HierarchicalEmbedsPlugin: We have markdown-embed ancestors", markdownEmbedAncestors);
+			}
+		}
+	});
+	markdownEmbedObserver.observe(
+		element,
+		{
+			subtree: true,
+			childList: true
+		}
+	);
+}
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
+function findAllMarkdownEmbedAncestors(element: HTMLElement) {
+	const ancestors: HTMLElement[] = [];
+	let currentElement: HTMLElement | null = element.parentElement;
+	while (currentElement) {
+		if (currentElement.classList.contains("markdown-embed")) {
+			ancestors.push(currentElement);
+		}
+		currentElement = currentElement.parentElement;
 	}
+	return ancestors;
+}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+function observeForMarkdownEmbed(element: HTMLElement) {
+	const internalEmbedObserver = new MutationObserver((records, observer) => {
+		for (const record of records) {
+			if (record.target instanceof HTMLElement &&
+				record.target.classList.contains("markdown-embed")) {
+				
+				observer.disconnect();
+				//console.debug("HierarchicalEmbedsPlugin: We have a markdown-embed!", record);
+				//console.debug("HierarchicalEmbedsPlugin: Does markdown-embed element have a grandparent?", record.target.parentElement?.parentElement);
+				observeForElementsAdded(record.target);
+				break;
+			}
+		}
+	});
+	internalEmbedObserver.observe(
+		element,
+		{
+			subtree: true,
+			attributes: true,
+			attributeFilter: ["class"],
+			attributeOldValue: true
+		}
+	);
 }
