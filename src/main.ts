654d13fcdcf9d5e104c5afefef868d6182afe77b
Brian Stepnitz <brian.stepnitz@gmail.com>
2026-01-15 14:21:26 -0500

    just need to get these changes to my laptop

--- src/main.ts
+++ src/main.ts
@@ -37,14 +37,20 @@ function observeForMarkdownEmbeds(internalEmbedSpanElement: Element) {
 		for (const record of records) {
 			if (isMarkdownEmbedRecord(record)) {
 				// Now we know it's a markdown embed, we can process it.
-				observer.disconnect(); // Stop observing once we've detected the change.
-				const markdownEmbedSpanElement = record.target as HTMLElement;
-				const markdownPreviewSection = markdownEmbedSpanElement.querySelector("div.markdown-preview-section");
-				
-				console.debug("HierarchicalEmbedsPlugin: Processing markdown embed", record.target);
 
-				break;
-			
+				//observer.disconnect(); // Stop observing once we've detected the change.
+
+				const markdownEmbedSpanElement = record.target as HTMLElement;
+
+				const markdownPreviewSection = markdownEmbedSpanElement.querySelector("div.markdown-preview-section")!;
+				observerForChildrenChanges(markdownPreviewSection);
+
+				// Get all <div> descendants WITHOUT any of the classes "markdown-preview-pusher", "mod-header", or "mod-ui".
+
+				// replaceWith() takes multiple arguments and replaces the element with all of them
+				//markdownEmbedSpanElement.closest("div")?.replaceWith(...Array.from(divDescendants));
+
+				//break;
 			}
 		}
 	});
@@ -56,4 +62,15 @@ function isMarkdownEmbedRecord(record: MutationRecord): boolean {
 		&& record.attributeName === "class"
 		&& record.target instanceof HTMLElement
 		&& record.target.matches(".markdown-embed");
+}
+
+function observerForChildrenChanges(element: Element) {
+	const mutationObserver = new MutationObserver((records, observer) => {
+		for (const record of records) {
+			if (record.type === "childList") {
+				console.debug("HierarchicalEmbedsPlugin: Child list changed:", record);
+			}
+		}
+	});
+	mutationObserver.observe(element, {childList: true});
 }
